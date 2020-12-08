c8768596ad7044f7832e62aae5ccff90976b5e80
muhammadidrees <idreesiqbal104@gmail.com>
2020-12-08 20:56:56 +0500

    calendar init

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import Slider from '../shared/Slider';
 import '../assets/css/home.css';
-// import { Link } from 'react-router-dom';
 
-class Home extends React.Component {
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
 
-    constructor(props) {
-        super(props);
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
-        this.state = {
-            data: [],
-            loading: false
-        };
-    } 
+function Home() {
 
-    componentDidMount() {
-        console.log("Home");
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
-        this.setState({
-            loading: true
-        });
-    }
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
 
-    render() {
-        return(
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-4">
-                        <div className="card my-5">
-                            <p>Home</p>
-                        </div>
+
+
+    return(
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-4">
+                    <div className="card my-5 px-3 py-2">
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <KeyboardDatePicker
+                                disableToolbar
+                                variant="inline"
+                                format="MM/dd/yyyy"
+                                margin="normal"
+                                id="date-picker-inline"
+                                label="Date picker inline"
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                            />
+                        </MuiPickersUtilsProvider>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 export default Home;
