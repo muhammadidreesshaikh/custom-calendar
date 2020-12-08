7a3a1013f09e0e1cf20a0a7d4e94a7da2076f3c0
muhammadidrees <idreesiqbal104@gmail.com>
2020-12-08 22:46:30 +0500

    Updates

---
+++ precache-manifest.7469130b5897bdbac79fec7977f50b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cf50d2e2312b70019497c122b94b54",
+    "url": "/custom-calendar/index.html"
+  },
+  {
+    "revision": "64c6f5076440a143d49a",
+    "url": "/custom-calendar/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "32f300c407368ab295ab",
+    "url": "/custom-calendar/static/css/main.b1b52e8f.chunk.css"
+  },
+  {
+    "revision": "64c6f5076440a143d49a",
+    "url": "/custom-calendar/static/js/2.fe328892.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/custom-calendar/static/js/2.fe328892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f300c407368ab295ab",
+    "url": "/custom-calendar/static/js/main.a97bde1f.chunk.js"
+  },
+  {
+    "revision": "bfe9c99d4f2dcf83e511",
+    "url": "/custom-calendar/static/js/runtime-main.f84f9ace.js"
+  }
+]);
